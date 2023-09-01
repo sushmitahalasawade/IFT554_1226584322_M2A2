b29498b96c0efd357383e9b86f3d80fde420e573
Sushmita Halasawade <shalasaw@asu.edu>
2023-09-01 03:05:59 -0700

    Video 2: Javascript Function refactoring completed

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 
-function greetTheStudent(studentName){ // parameter
-    // const message = 'Hello There';
-    // const greeting = message + ' ' + studentName;
-    return greeting =`Hello there ${studentName}`
-    // return greeting
+// function greetTheStudent(studentName){ // parameter // name function
+//     // const message = 'Hello There';
+//     // const greeting = message + ' ' + studentName;
+//     return greeting =`Hello there ${studentName}`;
+//     // return greeting
 
-}
+// }
+
+
+// const greetTheStudent = function (studentName){ // parameter // anonymous function
+//     return greeting =`Hello there ${studentName}`;
+
+// }
+
+// fat arrow function 
+// const greetTheStudent =  (studentName)=>{ // parameter // anonymous function
+//     return greeting =`Hello there ${studentName}`;
+
+// }
+const greetTheStudent = studentName => greeting =`Hello there ${studentName}`;
+
 // Do Not repeat Yourself (DRY)
 const student = 'Mary Jane';
 // const studentName2 = 'Mary Jane';
