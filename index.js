4b632bf6d35c41018d198fb44df8ca78d4d48599
Sushmita Halasawade <shalasaw@asu.edu>
2023-09-01 02:58:23 -0700

    video1 : Introduction to javascript completed

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function greetTheStudent(studentName){ // parameter
+    // const message = 'Hello There';
+    // const greeting = message + ' ' + studentName;
+    return greeting =`Hello there ${studentName}`
+    // return greeting
+
+}
+// Do Not repeat Yourself (DRY)
+const student = 'Mary Jane';
+// const studentName2 = 'Mary Jane';
+
+const greet =greetTheStudent(student); // argument
+console.log(greet);
+
+// console.log(message + ' ' + studentName);
+// console.log(message + ' ' + studentName2);
