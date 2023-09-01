8e5ef8c177b87a6f1271574d5796698b017c1bc4
Sushmita Halasawade <shalasaw@asu.edu>
2023-09-01 03:31:39 -0700

    Video 5: Javascript Objects completed

--- functionDemo1.js
+++ functionDemo1.js
@@ -1,12 +1,27 @@
 var age = 10; // int
 var salary = 15.00; // double
-console.log(`The variable type of age is ${typeof age}`);
-console.log(`The variable type of salary is ${typeof salary}`);
+// console.log(`The variable type of age is ${typeof age}`);
+// console.log(`The variable type of salary is ${typeof salary}`);
 
-var name = 'John Smith';
-console.log(`The variable type of name is ${typeof name}`);
+// var name = 'John Smith';
+// console.log(`The variable type of name is ${typeof name}`);
 
 const displayGreetings = function(name,year){
     console.log(`Happy new year ${year} ${name}`)
 }
-console.log(`The variable type of displayGreetings is ${typeof displayGreetings}`);
+// console.log(`The variable type of displayGreetings is ${typeof displayGreetings}`);
+
+const displayGreetingswithEmoji = function(name,year){
+    console.log(`😊 😊 Happy new year ${year} ${name} 😊 😊`)
+}
+
+var greet = {};
+
+if(age > 10){
+    greet = displayGreetings;
+}
+else{
+    greet = displayGreetingswithEmoji;
+}
+
+greet('Sam', 2022);
