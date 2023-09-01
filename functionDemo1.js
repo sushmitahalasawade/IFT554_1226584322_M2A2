a4ac8bae6751fb360f2c34c66678105672c9cd4d
Sushmita Halasawade <shalasaw@asu.edu>
2023-09-01 03:22:38 -0700

    Video 4: Javascript Variables completed

---
+++ functionDemo1.js
@@ -0,0 +1,12 @@
+var age = 10; // int
+var salary = 15.00; // double
+console.log(`The variable type of age is ${typeof age}`);
+console.log(`The variable type of salary is ${typeof salary}`);
+
+var name = 'John Smith';
+console.log(`The variable type of name is ${typeof name}`);
+
+const displayGreetings = function(name,year){
+    console.log(`Happy new year ${year} ${name}`)
+}
+console.log(`The variable type of displayGreetings is ${typeof displayGreetings}`);
